fa5423fda74b70c416bec4b69689f1da8359a331
Abhijeet Kalsi <abhijeet.kalsi@gmail.com>
2020-08-07 02:47:31 +0530

    Shell Script to RUN container in single click

--- bin/docker_start_local.sh
+++ bin/docker_start_local.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 # Run Local Docker Compose instance
 
-echo -e '\e[1;33m\nRunning LifePoints - Local Docker Container ...!\e[0m'
+echo -e '\e[1;33m\nRunning Drupal Docker Container ...!\e[0m'
 
 echo -e '\nSetting LP Environment variables...'
 
-export LP_PROJECT_NAME=lifepoints
-export PLATONE_STAGE=alpha
-export PLATONE_ENV=developer
-export DB_SUFFIX=_local
 
 #printenv
 
@@ -23,6 +19,6 @@ docker-compose -f ../docker-compose.yml up -d $*
 #docker-compose -f ../docker-compose.yml up --build $*
 
 # To verify Domain is reachable in Container.
-#curl -H Host:local.lifepointspanel.com http://127.0.0.1:80 $*
+#curl -H Host:localhost.drupal.docker.com http://127.0.0.1:80 $*
 
 $SHELL
